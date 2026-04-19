1cacc2af6b48f6bde30f33bb278f8e8d49d7899f
ags.stwn <ags.stwn@gmail.com>
2026-04-19 23:17:00 +0700

    Deploy langsung ke docs

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1537485364" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2933680193" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
