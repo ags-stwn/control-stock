0322b5d5ea15b78921874ead8c10bf40f19ecd34
ags.stwn <ags.stwn@gmail.com>
2026-05-05 22:36:54 +0700

    Deploy with correct base href

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1013150430" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3971113501" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
